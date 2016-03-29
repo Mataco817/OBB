ee95b12ea4b3cceb8762621434499eb21f4f2d31
Morabito <e313794@acct03.us.lmco.com>
2016-03-29 16:57:59 -0400

    Added shrinking toolbar to history tab

--- www/app/history/history.controller.js
+++ www/app/history/history.controller.js
@@ -3,36 +3,61 @@
 		.module('history')
 		.controller('HistoryController', HistoryController);
 	
-	HistoryController.$inject = ['$scope', 'databaseService', 'mongodbService'];
-	function HistoryController($scope, databaseService, mongodbService) {
+	HistoryController.$inject = ['$scope', '$timeout', '$document', 'databaseService', 'mongodbService'];
+	function HistoryController($scope, $timeout, $document, databaseService, mongodbService) {
 		var vm = this;
-		vm.sortingMethod = "Desc";
-		vm.records = [];
 		
+		vm.records = [];
+		vm.sortingMethod = "Desc";
+
+		vm.selectedOptionIndex = 3;
+		vm.optionClick = optionClick;
 		vm.options = [{
 			title : 'Select...',
 			icon : 'img/icons/check_circle.svg'
-		},
-		{
+		},{
 			title : 'Comfy View',
 			icon : 'img/icons/comfy_view.svg'
-		},
-		{
+		},{
 			title : 'Day View',
 			icon : 'img/icons/day_view.svg'
-		},
-		{
+		},{
 			title : 'Month View',
 			icon : 'img/icons/month_view.svg'
-		},
-		{
+		},{
 			title : 'Year View',
 			icon : 'img/icons/year_view.svg'
 		}];
 		
+		vm.testClass = {
+			"height" : "calc(100vh - 96px)"
+		};
+		
+		var content = angular.element(document.getElementById('scroll-content'));
+		
+		$scope.$watch(function watchContent(scope) {
+			return content[0].style["transform"];
+		},
+		function(newVal, oldVal) {				
+			var transformString = newVal;
+			var transformStringArray = transformString.split(" ");
+			var transformY = parseInt(transformStringArray[1]);
+			var heightOffset = 96 - (24 - transformY);
+			
+			vm.testClass["height"] = "calc(100vh - " + heightOffset.toString() + "px)";
+//			console.log(transformY);
+//			console.log("96 - (24 - " + transformY + ") = " + heightOffset);
+		});
+		
 		/*
 		 * Internal Methods
 		 */
+		function optionClick(index) {
+			$timeout(function() {
+				vm.selectedOptionIndex = index;
+			}, 500);
+		}
+		
 		function getMyRecords() {
 			mongodbService.getAllRecords()
 			.then(function(records) {
