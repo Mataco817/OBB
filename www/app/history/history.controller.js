5fd3aaecb50a94566bae1b77e23d5f0f1c0b4879
Morabito <e313794@acct03.us.lmco.com>
2016-04-07 11:32:42 -0400

    Condensed the required dynamic styling of the history items to directives

--- www/app/history/history.controller.js
+++ www/app/history/history.controller.js
@@ -3,19 +3,11 @@
 		.module('history')
 		.controller('HistoryController', HistoryController);
 	
-	HistoryController.$inject = ['$scope', '$timeout', '$rootScope', '$window', '$document', '$mdDialog', 'databaseService', 'mongodbService', 'utilService'];
-	function HistoryController($scope, $timeout, $rootScope, $window, $document, $mdDialog, databaseService, mongodbService, utilService) {
+	HistoryController.$inject = ['$scope', '$timeout', '$mdDialog', 'databaseService', 'mongodbService', 'utilService'];
+	function HistoryController($scope, $timeout, $mdDialog, databaseService, mongodbService, utilService) {
 		var vm = this;
 		var MONTH_VIEW = 'Month View';
 		var DAY_VIEW = 'Day View';
-		
-		vm.contentCSS = {
-			    "position": "absolute",
-			    "top": "56px",
-			    "bottom": "0px",
-			    "left": "0px",
-			    "right": "0px"
-		};
 		
 		vm.records = [];
 		vm.sortingMethod = "-";
@@ -45,37 +37,6 @@
 		vm.isDayView = function() { return isView(DAY_VIEW); };
 		vm.isMonthView = function() { return isView(MONTH_VIEW); };
 		
-		var offset = 0;
-		vm.applyClearFix = function(test) {
-			if (isView(MONTH_VIEW)) {
-				if (test.$parent.$first) {
-					offset = test.$parent.dayObj.records.length;
-					return test.$index % 6 === 0;
-				}
-				else {
-					var clearRow = (test.$index + offset) % 6 === 0;
-					
-					if (test.$last) {
-						offset += test.$parent.dayObj.records.length;
-					}
-					
-					return clearRow;
-				}
-			}
-			else if (isView(DAY_VIEW)) {
-				return test.$index % 4 === 0;
-			}
-		};
-		
-		vm.isNewRow = function(dayObj) {
-			if (isView(MONTH_VIEW)) {
-				return ((offset % 6 === 0) && (dayObj.records.length % 6 === 0));
-			}
-			else {
-				return true;
-			}
-		};
-		
 		/*
 		 * This will set the height of the content area relative to the amount
 		 * of the shrinking toolbar is shown/hidden
@@ -102,134 +63,21 @@
 		
 		/*
 		 * Internal Methods
-		 */
-		
-		vm.timer = 0;
-		$rootScope.currItemIndex = 0;
-		
+		 */		
 		$scope.$watch(function watchSorting(scope) {
 			return vm.sortingMethod;
 		},
 		function(newVal, oldVal) {
-			$window.clearTimeout(vm.timer);
-			vm.timer = $window.setTimeout(rearrange, 100);
+//			$window.clearTimeout(vm.timer);
+//			vm.timer = $window.setTimeout(rearrange, 100);
 		});
 		
 		$scope.$watch(function watchGrouping(scope) {
 			return vm.selectedOptionIndex;
 		},
 		function(newVal, oldVal) {
-			$window.clearTimeout(vm.timer);
-			vm.timer = $window.setTimeout(rearrange, 100);
-		});
-		
-		var lastWidth = -1;
-		var lastDay = -1;
-		var headerCount = 0;		
-		var indexOffset = 0;
-
-		var HEADER_SPACE = 63;
-		var ELEMENTS_PER_ROW = 0;
-		var ELEMENT_SIZE_WITH_PADDING = 108;
-		var HISTORY_CARD_WIDTH = 100;
-		var EXTRA_SPACE = 0;
-		var EXTRA_ELEMENT_WIDTH = 0;
-		
-		var lastHeadersChildren = 0;
-		var rowsAbove = 0;
-		function rearrange() {
-			var toolbarHeight = $('#history-toolbar').css("height");
-			vm.contentCSS["top"] = toolbarHeight;
-			
-			$('.history-card')
-			.each(function(idx, el){
-				if (idx === 0) {
-					headerCount = 0;
-					indexOffset = 0;
-					lastDay = -1;
-				}
-				
-				var $el = $(el);
-				var elementID = $el.attr("id");
-				var elementInfo = getElementInfo(elementID);
-				
-				var newRow = false;
-				if (elementInfo.day !== lastDay && elementInfo.index === 0) {
-					lastDay = elementInfo.day;
-					headerCount++;
-					newRow = true;
-				}
-				
-				var widthAvailable = el.parentElement.clientWidth;
-				
-				if (lastWidth !== widthAvailable) {
-					lastWidth = widthAvailable;
-					
-					/* Reset variables to match new window width */
-					ELEMENTS_PER_ROW = parseInt(widthAvailable / ELEMENT_SIZE_WITH_PADDING);
-					EXTRA_SPACE = widthAvailable - (ELEMENT_SIZE_WITH_PADDING * ELEMENTS_PER_ROW);
-					EXTRA_ELEMENT_WIDTH = parseInt(EXTRA_SPACE / ELEMENTS_PER_ROW);
-					
-					/* window will resize while DOM is loading, call rearrange again */
-					$window.clearTimeout(vm.timer);
-					vm.timer = $window.setTimeout(rearrange, 100);
-				}
-				
-				if (newRow) {
-					rowReset = (idx + indexOffset) % ELEMENTS_PER_ROW;
-					if (idx !== 0 && rowReset !== 0) {
-						indexOffset += (ELEMENTS_PER_ROW - rowReset);
-					}
-				}
-				
-				var newTop = parseInt((idx + indexOffset) / ELEMENTS_PER_ROW) * ELEMENT_SIZE_WITH_PADDING;
-				var newLeft = ((idx + indexOffset) % ELEMENTS_PER_ROW) * (ELEMENT_SIZE_WITH_PADDING + EXTRA_ELEMENT_WIDTH);
-
-				$el.css({
-					'width': HISTORY_CARD_WIDTH + EXTRA_ELEMENT_WIDTH,
-					'top' : newTop + (HEADER_SPACE * headerCount),
-					'left' : newLeft
-				});
-			});
-
-			$('h2.month-header')
-			.each(function(idx, el) {
-				if (idx === 0) {
-					headerCount = 0;
-					lastHeadersChildren = 0;
-					rowsAbove = 0;
-				}
-				
-				var $el = $(el);
-				var elementID = $el.attr("id");
-				var elementClass = $el.attr("class");
-				var elementChildren = parseInt(elementClass.split(' ')[0]);
-				
-				var newTop = rowsAbove * ELEMENT_SIZE_WITH_PADDING;
-				$el.css({
-					'top' : newTop + (HEADER_SPACE * idx)
-				});
-				
-				lastHeadersChildren = elementChildren;
-				rowsAbove += Math.ceil(lastHeadersChildren / ELEMENTS_PER_ROW);
-			});
-		}
-		
-		function getElementInfo(elementIDString) {
-			var tempArray = elementIDString.split('-');
-			return {
-				year : parseInt(tempArray[0]),
-				month : parseInt(tempArray[1]),
-				day : parseInt(tempArray[2]),
-				index : parseInt(tempArray[3])
-			};
-		}
-		
-		$(document).ready(function() {
-			$(window).resize(function() {
-				$window.clearTimeout(vm.timer);
-				vm.timer = $window.setTimeout(rearrange, 100);
-			});
+//			$window.clearTimeout(vm.timer);
+//			vm.timer = $window.setTimeout(rearrange, 100);
 		});
 		
 		function optionClick(index) {
@@ -289,26 +137,9 @@
 			mongodbService.getAllRecords()
 			.then(function(records) {
 				var years = [];
-				var yearIndexes = {};				
+				var yearIndexes = {};
 				
-				var tempArray = [];
-				var todayArray = [];
 				for (var i = 0; i < records.length; i++) {
-//					if (records[i].name === "OB Test") {
-//						var dateStr = records[i].date;
-//						var m = moment(dateStr, "YYYY:MM:DD:HH:mm:ss", true);
-//						records[i].day = utilService.getDays()[m.day()];
-//						records[i].month = utilService.getMonths()[m.month()];
-//						records[i].monthDay = m.date();
-//						records[i].year = m.year();
-//						records[i].moment = m;
-//
-//						records[i].weight = parseInt(records[i].weight);
-//						records[i].RPE = parseInt(records[i].RPE);
-//						
-//						tempArray.push(records[i]);
-//					}
-
 					var timeObj = new Date(records[i].time);
 					var m = moment(timeObj);
 					records[i].dateObj = timeObj;
@@ -367,15 +198,9 @@
 					var monthIndex = yearIndexes[records[i].year].monthIndexes[records[i].monthNumber].monthIndex;
 					var dayIndex = yearIndexes[records[i].year].monthIndexes[records[i].monthNumber].dayIndexes[records[i].monthDay].dayIndex;
 					years[yearIndex].months[monthIndex].days[dayIndex].records.push(records[i]);
-					
-//					tempArray.push(records[i]);
 				}
-				
-//				vm.records = records;
-//				vm.records = tempArray;
+
 				vm.records = years;
-				
-				rearrange();
 			});
 		}
 
